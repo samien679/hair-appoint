0bf859a4cc2a6bb456e7541a9961de83174330e2
Sami Enholm <sami.enholm@gmail.com>
2023-12-26 19:40:26 +0200

    feat: new calendar block component

---
+++ resources/js/Components/StaffCalendarBlock.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+//defineProps({ date: String })
+
+</script>
+<template>
+    <div class="bg-green-400 border border-gray-700 border-1">
+        <div class="px-2 ">
+            Test
+        </div>
+
+
+    </div>
+</template>
--- resources/js/Pages/MySchedule.vue
+++ resources/js/Pages/MySchedule.vue
@@ -1,6 +1,8 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import StaffDayHeader from '@/Components/StaffDayHeader.vue';
+import StaffCalendarBlock from '@/Components/StaffCalendarBlock.vue';
+
 defineProps({ date: Object })
 </script>
 
@@ -18,18 +20,16 @@ defineProps({ date: Object })
                     <div
                         class="relative flex min-h-screen bg-gray-100 bg-center bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
                         <div class="w-8 h-full bg-green-300">test</div>
-                        <div class="grid h-4 grid-cols-7 auto-cols-auto">
-                            <StaffDayHeader :date="date.day1" />
-                            <StaffDayHeader :date="date.day2" />
-                            <StaffDayHeader :date="date.day3" />
-                            <StaffDayHeader :date="date.day4" />
-                            <StaffDayHeader :date="date.day5" />
-                            <StaffDayHeader :date="date.day6" />
-                            <StaffDayHeader :date="date.day7" />
+                        <div>
+                            <div class="grid grid-cols-7 mb-10">
+                                <StaffDayHeader :date="date" :v-for="date in date" :key="date.date" />
 
+                            </div>
+                            <div class="grid grid-cols-7" :v-for="date in date">
+                                <StaffCalendarBlock />
+                            </div>
                         </div>
-                        <div class="grid grid-cols-7 grid-rows-6">
-                        </div>
+
                     </div>
                 </div>
             </div>
